30b037c41f4278737f06d3520d295df9569c295b
Cheryl Lee <99976292+CherylL23@users.noreply.github.com>
2022-03-30 13:25:12 +0000

    push

--- Sketch.java
+++ Sketch.java
@@ -2,7 +2,6 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -24,7 +23,7 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
+	  // sample code, delete this stuff
     /*
     stroke(128);
     line(150, 25, 270, 350);  
@@ -74,10 +73,11 @@ public class Sketch extends PApplet {
     int intX = 0;
     int intY = 0;
 
+    //filling the entire section with evenly spaced squares
     for(int intRow = 0; intRow < 30; intRow++){
       for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+        intX = 3 + intRow * 10;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY = 300 + 3 + intColumn * 10; //Instead of zero, calculate the proper intY location using 'intColumn'
 
         fill(255);
         noStroke();
@@ -92,7 +92,26 @@ public class Sketch extends PApplet {
    * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
    */
   public void draw_section2(){
+    int intX2 = 0;
+    int intY2 = 0;
 
+    //filling the entire section with evenly spaced squares
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX2 = 300 + 3 + intRow * 10;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY2 = 300 + 3 + intColumn * 10; //Instead of zero, calculate the proper intY location using 'intColumn'
+
+        if((intRow % 2) == 0){
+          fill(255, 255, 255);
+        }
+        else {
+          fill(0, 0, 0);
+        }
+        
+        noStroke();
+        rect(intX2, intY2, 5, 5);
+      }
+    }
   }
 
   /**
@@ -100,21 +119,76 @@ public class Sketch extends PApplet {
    * Don't use multiple 'if' statements.
    */
   public void draw_section3(){
+    int intX3 = 0;
+    int intY3 = 0;
 
+    //filling the entire section with evenly spaced squares
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX3 = 600 + 3 + intRow * 10;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY3 = 300 + 3 + intColumn * 10; //Instead of zero, calculate the proper intY location using 'intColumn'
+
+        if((intColumn % 2) == 0){
+           fill(0, 0, 0);
+        }
+        else {
+         fill(255, 255, 255);
+        }
+        
+        noStroke();
+        rect(intX3, intY3, 5, 5);
+      }
+    }
   }
 
   /**
    * Use the modulus operator and just one 'if' statement to select the color.
    */
   public void draw_section4(){
+   
+    int intX4 = 0;
+    int intY4 = 0;
+    
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX4 = 900 + 3 + intRow * 10;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY4 = 300 + 3 + intColumn * 10; //Instead of zero, calculate the proper intY location using 'intColumn'
 
+        if((intColumn % 2) != 0 && (intRow % 2) == 0){
+           fill(255, 255, 255);
+        }
+        else {
+         fill(0, 0, 0);
+        }
+        
+        noStroke();
+        rect(intX4, intY4, 5, 5);
+      }
+    }
   }
 
   /**
    * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
    */
   public void draw_section5(){
+    int intX5 = 0;
+    int intY5 = 0;
+    int rowCount5 = 0;
+    int columnCount5 = 0;
+    
 
+    //filling the entire section with evenly spaced squares
+    for(int intRow5 = 30; rowCount5 < intRow5; intRow5--){
+      for(int intColumn5 = 30; columnCount5 < intColumn5 ; intColumn5--){
+        intX5 = 3 - 10 + (intRow5 * 300 / 30);  
+        intY5 = 3 + (intColumn5 * 300 / 30); 
+
+        fill(255);
+        noStroke();
+        rect(intX5, intY5, 5, 5);
+
+      }
+    }
   }
 
   public void draw_section6(){
